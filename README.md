49bba59ab881afda8cdac821ee80f0769d4c1944
Tao Hu <taohu88@gmail.com>
2019-07-20 15:47:26 -0700

    first checked in

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # primus
+
+Primus is a collections of bunch useful feature transformers especially for pandas.
---
+++ setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+#
+# Some basic setup for now
+#
+from setuptools import setup, find_packages
+
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name = "primus",
+    version = "0.0.1",
+    author = "Tao Hu",
+    author_email = "taohu88@gmail.com",
+    description = ("prims, a collection of useful transformers especially for pandas"),
+    license = license,
+    keywords = "machine learning feature engineering",
+    url = "https://github.com/taohu88/primus",
+    packages=find_packages(),
+    long_description=readme,
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3.7',
+        "Topic :: App framework",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
